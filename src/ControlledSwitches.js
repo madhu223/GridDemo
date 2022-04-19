ba71ac01bb5a86fe6605718e7ff90d4a87f9fa49
madhu223 <madhumorigadi@gmail.com>
2022-04-19 23:58:19 +0530

    pushing grid changes

---
+++ src/ControlledSwitches.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import Switch from '@mui/material/Switch';
+
+export default function ControlledSwitches() {
+  const [checked, setChecked] = React.useState(true);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+    console.log(event.target.value)
+  };
+
+  return (
+    <Switch
+      checked={checked}
+      onChange={handleChange}
+      inputProps={{ 'aria-label': 'controlled' }}
+    />
+  );
+}
